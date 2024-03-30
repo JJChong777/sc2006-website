48d4f5e413a82cf3b2f42539cee43776cd295376
JJChong777 <jiejun815@gmail.com>
2024-03-30 09:27:19 +0800

    change welcome text when logged in

--- bookworm/app/page.jsx
+++ bookworm/app/page.jsx
@@ -36,7 +36,7 @@ export default function main() {
       {isAuthenticated && (
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Welcome, you are
+            You're
           </h2>
           <h2 className="text-4xl font-bold tracking-tight text-blue-400 sm:text-6xl">
             logged in!
