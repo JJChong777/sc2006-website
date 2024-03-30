6c63f19d4de31cae872027150be2c4b1fa662664
tonasyonas <jonas.tua@gmail.com>
2024-03-30 21:03:06 +0800

    changed landing page abit

--- bookworm/app/page.jsx
+++ bookworm/app/page.jsx
@@ -46,9 +46,12 @@ export default function main() {
             </span>
           </h2>
 
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            Explore our app with the navbar above!
-          </p>
+          <Link
+            href="/search"
+            className="mt-8 inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Search for a book now!
+          </Link>
         </div>
       )}
     </div>
