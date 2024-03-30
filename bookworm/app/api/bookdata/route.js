f6f3b6dacdb7f75c7e7d0e37f2e79c61eb9ed80f
JJChong777 <jiejun815@gmail.com>
2024-03-30 14:26:25 +0800

    update bookdata api to give cover, edit landing page and signup
    - now welcomes each user based on the first part of the email, (before the @)
    - let bookdata api have the cover of each image under attr bookCover
    - comment out username during signup because I think there is no point having it

--- bookworm/app/api/bookdata/route.js
+++ bookworm/app/api/bookdata/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import fetch from "node-fetch";
 require("dotenv").config();
 
-async function getBooks() {
+async function getBooks(keyword) {
   const myHeaders = new Headers();
   myHeaders.append("X-Api-Key", process.env.NLB_API_KEY);
   myHeaders.append("X-App-Code", process.env.NLB_APP_CODE);
@@ -12,7 +12,7 @@ async function getBooks() {
     redirect: "follow",
   };
   const response = await fetch(
-    "https://openweb.nlb.gov.sg/api/v2/Catalogue/GetTitles?Keywords=Divergent",
+    `https://openweb.nlb.gov.sg/api/v2/Catalogue/GetTitles?Keywords=${keyword}`,
     requestOptions
   );
 
@@ -21,11 +21,19 @@ async function getBooks() {
   return data;
 }
 
-export async function GET() {
+export async function GET(request) {
   const bookData = [];
-  const data = await getBooks();
+  const keyword = request.nextUrl.searchParams.get("keyword");
+  if (!keyword)
+    return NextResponse.json({
+      error: "No keyword specified, try ?keyword={book name here}",
+    });
+  const data = await getBooks(keyword);
   data["titles"].forEach((resource) => {
     if (resource["format"]["name"] === "Book") {
+      resource["bookCover"] = `https://covers.openlibrary.org/b/isbn/${resource[
+        "isbns"
+      ][0].slice(0, 13)}-L.jpg`;
       bookData.push(resource);
     }
   });
