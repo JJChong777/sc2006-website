187738b796efa5e4fa0da8ed055f5341ee242368
JJChong777 <jiejun815@gmail.com>
2024-04-04 19:05:46 +0800

    favicon must work now, I believe

--- bookworm/app/layout.jsx
+++ bookworm/app/layout.jsx
@@ -3,12 +3,6 @@ import { AuthProvider } from "../app/auth/authentication_functions/AuthContext";
 import "dotenv/config";
 import Navbar from "./components/Navbar";
 
-export const metadata = {
-  icons: {
-    icon: "/favicon.ico", // /public path
-  },
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
