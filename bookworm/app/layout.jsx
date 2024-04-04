46bc85a84f94894b59dba27f08dec9dad6b77e77
JJChong777 <jiejun815@gmail.com>
2024-04-04 18:58:51 +0800

    delete unnecessary images, make the favicon work
    - yes I could implement actually useful functions like delete saved book and delete genre but that sounds to tedious
    - but why not delete unnecessary images used in the sample data and try to make the favicon work instead
    - because everyone needs to see a tiny bookworm image (true)

--- bookworm/app/layout.jsx
+++ bookworm/app/layout.jsx
@@ -3,12 +3,18 @@ import { AuthProvider } from "../app/auth/authentication_functions/AuthContext";
 import "dotenv/config";
 import Navbar from "./components/Navbar";
 
+// export const metadata = {
+//   icons: {
+//     icon: "/favicon.ico", // /public path
+//   },
+// };
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <AuthProvider>
-          <Navbar/>
+          <Navbar />
           {children}
         </AuthProvider>
       </body>
