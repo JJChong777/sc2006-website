9dcc86b7de5793cb82e38e013548b6d86f367097
tonasyonas <jonas.tua@gmail.com>
2024-04-11 12:55:06 +0800

    rm forgot password
    nil

--- bookworm/app/login/page.jsx
+++ bookworm/app/login/page.jsx
@@ -65,14 +65,7 @@ export default function Login() {
                 >
                   Password
                 </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
+                <div className="text-sm"></div>
               </div>
               <div className="mt-2">
                 <input
@@ -86,7 +79,6 @@ export default function Login() {
                     setPassword(e.target.value);
                   }}
                   placeholder="your password"
-
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
                 />
               </div>
