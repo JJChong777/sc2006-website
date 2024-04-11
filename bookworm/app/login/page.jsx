3a6f82b33e434531ffca30cc4c0793ec6b770655
tonasyonas <jonas.tua@gmail.com>
2024-04-11 12:54:01 +0800

    added placeholder for password
    nil

--- bookworm/app/login/page.jsx
+++ bookworm/app/login/page.jsx
@@ -85,6 +85,8 @@ export default function Login() {
                   onChange={(e) => {
                     setPassword(e.target.value);
                   }}
+                  placeholder="your password"
+
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
                 />
               </div>
