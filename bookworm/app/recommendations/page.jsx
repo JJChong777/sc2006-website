cb4e7e973bc07821393d5834d6a6d642b8a485a8
tonasyonas <jonas.tua@gmail.com>
2024-04-11 20:13:49 +0800

    rm comments
    nil

--- bookworm/app/recommendations/page.jsx
+++ bookworm/app/recommendations/page.jsx
@@ -3,100 +3,12 @@ import { useState, useEffect } from "react";
 import { useAuth } from "../auth/authentication_functions/AuthContext";
 import { addBookToDatabaseRec } from "./helper/addbookrec";
 
-// Sample book data
-// const initialBooks = [
-//   {
-//     id: 1,
-//     title: "Harry Potter and the Sorcerer’s Stone",
-//     author: "J.K. Rowling",
-//     description:
-//       "The first book in the Harry Potter series, introducing Harry Potter, a young wizard, and his adventures at Hogwarts School of Witchcraft and Wizardry.",
-//     image: "/harrypotterandsorcerer.jpg", // Correct the file name if necessary
-//   },
-//   {
-//     id: 2,
-//     title: "Divergent",
-//     author: "Veronica Roth",
-//     description:
-//       "A thrilling dystopian novel about a society divided into five factions, where Tris Prior must choose her place and face the consequences.",
-//     image: "/divergent.jpg", // Correct the file name if necessary
-//   },
-//   {
-//     id: 3,
-//     title: "The Hobbit",
-//     author: "J.R.R. Tolkien",
-//     description:
-//       "The adventure of Bilbo Baggins as he journeys to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home from a dragon named Smaug.",
-//     image: "/thehobbitt.jpg", // Correct the file name if necessary
-//   },
-//   {
-//     id: 4,
-//     title: "Insurgent",
-//     author: "Veronica Roth",
-//     description:
-//       "The second installment in the Divergent series, following Tris as she fights against a powerful alliance that threatens to tear her society apart.",
-//     image: "/insurgent.jpg", // Correct the file name if necessary
-//   },
-//   {
-//     id: 5,
-//     title: "Four",
-//     author: "Veronica Roth",
-//     description:
-//       'A companion volume to the Divergent series, telling the story from the perspective of Tobias Eaton, known as "Four".',
-//     image: "/four.jpg", // This is the correct path as you indicated you have this image
-//   },
-//   {
-//     id: 6,
-//     title: "Harry Potter and the Philosopher’s Stone",
-//     author: "J.K. Rowling",
-//     description:
-//       "The British version of the first book in the Harry Potter series, where the young wizard Harry Potter discovers his magical heritage and attends Hogwarts.",
-//     image: "/harrypotterandphilostone.jpg", // Correct the file name if necessary
-//   },
-// ];
-
-// const BookCard = ({ book }) => {
-//   return (
-
-//   );
-// };
-
 const BookRecommendations = () => {
   const [books, setBooks] = useState([]);
   const [error, setError] = useState("");
   // const [availabilityData, setAvailabilityData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const { userData } = useAuth();
-
-  // useEffect(() => {
-  //   const fetchAvailability = async () => {
-  //     const availabilityData = {};
-
-  //     // Fetch availability for each book
-  //     for (const book of books) {
-  //       const isbn = book.ISBN.trim();
-  //       try {
-  //         const response = await fetch(`/api/availability?ISBN=${isbn}`);
-  //         if (!response.ok) throw new Error("Network response was not ok.");
-  //         const data = await response.json();
-  //         availabilityData[isbn] = [];
-  //         if (data.items.length <= 0) availabilityData[isbn] = null;
-  //         data.items.forEach((item) =>
-  //           availabilityData[isbn].push(item["location"]["name"])
-  //         );
-  //       } catch (error) {
-  //         console.error("Error fetching availability:", error);
-  //         availabilityData[isbn] = null;
-  //       }
-  //     }
-
-  //     setAvailabilityData(availabilityData);
-  //   };
-
-  //   if (books.length) {
-  //     fetchAvailability();
-  //   }
-  // }, [books]); // Dependency array, re-run the effect if books changes
 
   const generateRecommendations = async () => {
     setIsLoading(true);
