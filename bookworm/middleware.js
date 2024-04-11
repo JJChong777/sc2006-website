8c777ed5a2738f965dd947821dd93eb3432f443c
JJChong777 <jiejun815@gmail.com>
2024-04-11 23:51:48 +0800

    fak the protected route thing

--- bookworm/middleware.js
+++ bookworm/middleware.js
@@ -3,38 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   const res = NextResponse.next();
-  const publicUrls = [
-    "/",
-    "/forgotpassword",
-    "/login",
-    "/reset",
-    "/map",
-    "/signup",
-    "/search",
-  ];
-  if (publicUrls.includes(req.nextUrl.pathname)) {
-    return res;
-  }
   const supabase = createMiddlewareClient({ req, res });
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    return NextResponse.rewrite(new URL("/login", req.url));
-  }
+  await supabase.auth.getSession(); // refresh active session
+  return res;
 }
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
-};
